9a7c927ad1f1972d8a6d55825df7a6db8372e68b
Khawla Boukniter <boukniter.khawla@gmail.com>
2026-03-02 12:29:55 +0000

    architechture

---
+++ discovery-server/discovery/discovery/src/test/java/com/mediahub/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediahub.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
