081e3e7e839925d1cb94a7c7c68e1a2a06fee383
asma828 <asmalachhab66@gmail.com>
2026-03-04 10:37:41 +0000

    change the module descovery

--- discovery-server/discovery1/src/main/java/org/example/App.java
+++ discovery-server/discovery1/src/main/java/org/example/App.java
@@ -1,16 +1,19 @@
-package com.mediahub.discovery;
+package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-
+/**
+ * Hello world!
+ *
+ */
 @SpringBootApplication
 @EnableEurekaServer
-public class DiscoveryApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DiscoveryApplication.class, args);
-	}
-
+public class App 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+    }
 }
