bb59c11d234e84cb30e36cf5c206af938cd9550f
asma828 <asmalachhab66@gmail.com>
2026-03-04 10:00:27 +0000

    add prod configuration

--- discovery-server/discovery/discovery/src/main/java/com/mediahub/discovery/DiscoveryApplication.java
+++ discovery-server/discovery/discovery/src/main/java/com/mediahub/discovery/DiscoveryApplication.java
@@ -2,8 +2,11 @@ package com.mediahub.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryApplication {
 
 	public static void main(String[] args) {
