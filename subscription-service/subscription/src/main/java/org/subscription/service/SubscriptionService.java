d187357589c6668c2ed407784418d5a06bd922d8
Khawla Boukniter <boukniter.khawla@gmail.com>
2026-03-05 12:00:00 +0000

    Update SubscriptionService interface for per-media subscription

--- subscription-service/subscription/src/main/java/org/subscription/service/SubscriptionService.java
+++ subscription-service/subscription/src/main/java/org/subscription/service/SubscriptionService.java
@@ -5,6 +5,6 @@ import org.subscription.model.Subscription;
 import java.util.List;
 
 public interface SubscriptionService {
-    public Subscription createSubscription(Long userId);
+    public Subscription createSubscription(Long userId, Long mediaId);
     public List<Subscription> getAllSubscriptions();
 }
