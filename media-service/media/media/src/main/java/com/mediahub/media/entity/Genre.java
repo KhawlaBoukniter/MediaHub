cbc7c87031a2b816b3052f375079da8217089e23
majdifox <majmehdi20@gmail.com>
2026-03-12 12:09:39 +0000

    creating the genre enum for the Media

---
+++ media-service/media/media/src/main/java/com/mediahub/media/entity/Genre.java
@@ -0,0 +1,14 @@
+package com.mediahub.media.entity;
+
+public enum Genre {
+    ACTION,
+    COMEDY,
+    DRAMA,
+    HORROR,
+    SCI_FI,
+    DOCUMENTARY,
+    THRILLER,
+    ROMANCE,
+    ANIMATION,
+    FANTASY
+}
